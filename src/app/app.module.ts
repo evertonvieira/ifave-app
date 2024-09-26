89aa0cce8cd26b7b6ec3229252d176cdaf6581bf
Everton Vieira <evertonvieira1994@gmail.com>
2024-09-26 17:51:18 -0300

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { HomePage } from './pages/home/home.page';
 
 @NgModule({
-  declarations: [AppComponent, HomePage],
-  imports: [
-    BrowserModule, 
-    IonicModule.forRoot(), 
-    AppRoutingModule,
-    CommonModule,
-    FormsModule,
-    IonicModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
